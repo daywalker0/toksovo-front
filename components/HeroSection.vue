6d03a8ea9529f8f940ea50113757fa26dc8d8693
daywalker0 <max1m.ar@yandex.ru>
2025-10-20 17:56:48 +0300

    pravki

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -138,13 +138,13 @@ onBeforeUnmount(() => {
 
 .hero-section {
   position: relative;
-  min-height: 210vh; // соответствует длине анимации
+  min-height: 250vh; // соответствует длине анимации
   overflow: hidden;
 
   &__container {
     position: relative;
     z-index: 2; // контент над фоном
-    min-height: 210vh;
+    min-height: 200vh;
     display: flex;
     align-items: flex-start;
     justify-content: center;
